04ca5dd188796de0c06c6bbff63c0c1825ac4963
Ryo Hirayama <ryo12redstone@gmail.com>
2022-02-05 19:58:00 +0900

    Fix

--- src/systems/wasm.rs
+++ src/systems/wasm.rs
@@ -3,13 +3,11 @@ use bevy::prelude::*;
 const WIDTH: f32 = 600.0;
 
 pub fn resize(mut windows: ResMut<Windows>) {
-    if windows.is_changed() {
-        let js_window = web_sys::window().unwrap();
-        let window = windows.get_primary_mut().unwrap();
-        let width = js_window.inner_width().unwrap().as_f64().unwrap() as f32;
-        let height = js_window.inner_height().unwrap().as_f64().unwrap() as f32;
-        window.set_resolution(width * (WIDTH / width), height * (WIDTH / width));
-    }
+    let js_window = web_sys::window().unwrap();
+    let window = windows.get_primary_mut().unwrap();
+    let width = js_window.inner_width().unwrap().as_f64().unwrap() as f32;
+    let height = js_window.inner_height().unwrap().as_f64().unwrap() as f32;
+    window.set_resolution(width * (WIDTH / width), height * (WIDTH / width));
 }
 
 #[derive(PartialEq)]
