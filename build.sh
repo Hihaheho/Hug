28db83a67c53f6517b8dda9ce7361f40a067a863
Ryo Hirayama <ryo12redstone@gmail.com>
2022-12-10 00:43:15 +0900

    Use heroku

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --target wasm32-unknown-unknown --release --features web &&
-wasm-bindgen --out-dir .public --target web target/wasm32-unknown-unknown/release/hug.wasm
+~/.cargo/registry/bin/wasm-bindgen-0.2.64/bin/wasm-bindgen --out-dir .public --target web target/wasm32-unknown-unknown/release/hug.wasm
