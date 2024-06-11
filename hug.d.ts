50e02777f714c01ad44d676a537538d217115f3c
ryo33 <ryo33@users.noreply.github.com>
2024-06-11 13:36:59 +0000

    Deploying to gh-pages from @ Hihaheho/Hug@8407cccf023f1a9b09cfed8b8e9fb12c7d76ba4d 🚀

--- hug.d.ts
+++ hug.d.ts
@@ -31,9 +31,9 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly wasm_bindgen__convert__closures__invoke1_mut__h1a6c65301dfa05d5: (a: number, b: number, c: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke0_mut__h913bb59da714ffac: (a: number, b: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke1_mut__h62bd1ae715296cd2: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h17a9c5e67a205047: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h5db3315855011f36: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc2c8e2d3b223e248: (a: number, b: number, c: number) => void;
   readonly __wbindgen_free: (a: number, b: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
   readonly __wbindgen_start: () => void;
