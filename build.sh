ba3756feac46aab6c7cb470188efdfea3fa0eb91
Ryo Hirayama <ryo12redstone@gmail.com>
2024-11-28 23:11:21 +0900

    From 2024

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+set -e
 RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --target wasm32-unknown-unknown --release --features web &&
-~/.cargo/registry/bin/wasm-bindgen-0.2.64/bin/wasm-bindgen --out-dir .public --target web target/wasm32-unknown-unknown/release/hug.wasm
+wasm-bindgen --out-dir .public --target web target/wasm32-unknown-unknown/release/hug.wasm
